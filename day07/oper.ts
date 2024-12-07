2044436d7573c70a632fcc87b705c1ae0eb1a107
Brett Kiefer <kiefer@gmail.com>
2024-12-07 11:51:31 -1000

    Advent of Code Day 7 solutions in Deno

---
+++ day07/oper.ts
@@ -0,0 +1,21 @@
+const lines = Deno.readTextFileSync(Deno.args[0])
+  .split(/\n/)
+  .filter((x) => x.length > 0)
+  .map((line) => line.split(/[:? ]+/).map((x) => parseInt(x)));
+
+function evalAll(xs: number[]): number[] {
+  if (xs.length === 1) {
+    return xs;
+  }
+
+  const x = xs[0];
+  const rest = xs.slice(1);
+  const subs = evalAll(rest);
+  return subs.map((sub) => x + sub).concat(subs.map((sub) => x * sub));
+}
+
+const valid = lines.filter((line) => {
+  return evalAll(line.slice(1).reverse()).indexOf(line[0]) > -1;
+});
+
+console.log(valid.reduce((acc, xs) => acc + xs[0], 0));
