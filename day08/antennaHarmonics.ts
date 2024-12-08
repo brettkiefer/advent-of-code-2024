e90b7425df891727881e2d2621aa61b87f08271a
Brett Kiefer <kiefer@gmail.com>
2024-12-07 23:29:07 -1000

    Advent of Code Day 8 solutions in Deno

---
+++ day08/antennaHarmonics.ts
@@ -0,0 +1,61 @@
+const text = Deno.readTextFileSync(Deno.args[0]);
+const lines = text.split(/\n/)
+  .filter((x) => x.length > 0)
+  .map((line) => line.split(""));
+
+const antennaMap: Map<string, number[][]> = new Map();
+
+for (let x = 0; x < lines.length; x++) {
+  for (let y = 0; y < lines[x].length; y++) {
+    const c = lines[x][y];
+    if (c !== ".") {
+      const antennas = antennaMap.get(c) ?? [];
+      antennas.push([x, y]);
+      antennaMap.set(c, antennas);
+    }
+  }
+}
+
+function aboutEqual(a: number, b: number): boolean {
+  return Math.abs(a - b) < 0.00000001;
+}
+
+function distance([ax, ay]: number[], [bx, by]: number[]): number {
+  return Math.sqrt(Math.pow(ax - bx, 2) + Math.pow(ay - by, 2));
+}
+
+function inLine(a: number[], b: number[], c: number[]): boolean {
+  const ps = [a, b, c].sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  });
+  return aboutEqual(
+    distance(ps[0], ps[1]) + distance(ps[1], ps[2]),
+    distance(ps[0], ps[2]),
+  );
+}
+
+const antiNodes = [];
+for (let x = 0; x < lines.length; x++) {
+  for (let y = 0; y < lines[x].length; y++) {
+    if (
+      Array.from(antennaMap).some(([, locs]) => {
+        for (let l1 = 0; l1 < locs.length; l1++) {
+          for (let l2 = l1 + 1; l2 < locs.length; l2++) {
+            if (inLine(locs[l1], locs[l2], [x, y])) {
+              return true;
+            }
+          }
+        }
+        return false;
+      })
+    ) {
+      antiNodes.push([x, y]);
+    }
+  }
+}
+
+console.log(antiNodes);
+console.log(antiNodes.length);
